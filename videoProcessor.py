5d038da70869f1f80a3479ea03056443f72b4a83
Jose <ignacioalfaro99@gmail.com>
2021-09-26 15:30:29 -0600

    CambiosMínimos

--- videoProcessor.py
+++ videoProcessor.py
@@ -3,39 +3,48 @@ import cv2
 import math
 import os
 from os import mkdir
- # Si la carpeta no está creada, crearla. 
 
-pathFile = 'D:\TEC\SO\primerProyecto\VideoProcessor\\videos\disparos.mp4'
+
+
+pathFile = '/home/nacho/Desktop/videos/bb.mp4'
 
 def videoProcessor( pathFile ): 
+    
     count = 0
     videoFile = pathFile
     cap = cv2.VideoCapture( videoFile )
     frameRate =  cap.get(5)
 
     while( cap.isOpened() ):
+
       frameId = cap.get(1)
       ret, frame = cap.read()
-      if( ret != True ):
+
+      if not ret:
         break
+
+
       if (frameId % math.floor(frameRate) == 0):
 
-        pathToSave = pathFile.split("\\")[-1][0:-4]
+        video_file_name = pathFile.split("/")[-1][0:-4]
         
-        if( os.path.isdir('./unstructuredVideos/'+pathToSave)):
-
-          filename = './unstructuredVideos/'+pathToSave+"/video%d.jpg" % count;count+=1
-          print( filename )
-          cv2.imwrite(filename, frame)
-
+        if( os.path.isdir('./unstructuredVideos/' + video_file_name)):
+          writeFile(video_file_name, count, frame)
+          count+=1
         else:
-          mkdir('./unstructuredVideos/'+pathToSave)
-          
-          filename = './unstructuredVideos/'+pathToSave+"/video%d.jpg" % count;count+=1
-          print( filename )
-          cv2.imwrite(filename, frame)
-          
+
+          mkdir('./unstructuredVideos/' + video_file_name)
+          writeFile(video_file_name, count, frame)      
+          count += 1
 
     cap.release()
+
+
+
+def writeFile( video_file_name,count, frame):
+  filename = './unstructuredVideos/'+video_file_name+"/" + video_file_name +"%d.jpg" % count;
+  print(filename)
+  cv2.imwrite(filename, frame)
+
 
 videoProcessor( pathFile )
