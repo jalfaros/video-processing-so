a9fe46c044d59c5d0cfa21e8ae1a975818de8336
warner hurtado laguna <warner3898@gmail.com>
2021-09-26 15:13:54 -0600

    agregando rutas de las imagenes

--- videoProcessor.py
+++ videoProcessor.py
@@ -1,8 +1,11 @@
+from genericpath import isdir
 import cv2
 import math
+import os
+from os import mkdir
  # Si la carpeta no está creada, crearla. 
 
-pathFile = '/home/nacho/Desktop/VideoProcessor/videos/kof.mp4'
+pathFile = 'D:\TEC\SO\primerProyecto\VideoProcessor\\videos\disparos.mp4'
 
 def videoProcessor( pathFile ): 
     count = 0
@@ -10,8 +13,6 @@ def videoProcessor( pathFile ):
     cap = cv2.VideoCapture( videoFile )
     frameRate =  cap.get(5)
 
-
-    x = 1
     while( cap.isOpened() ):
       frameId = cap.get(1)
       ret, frame = cap.read()
@@ -19,15 +20,22 @@ def videoProcessor( pathFile ):
         break
       if (frameId % math.floor(frameRate) == 0):
 
-          filename ="/home/nacho/Desktop/VideoProcessor/images/video%d.jpg" % count;count+=1
+        pathToSave = pathFile.split("\\")[-1][0:-4]
+        
+        if( os.path.isdir('./unstructuredVideos/'+pathToSave)):
+
+          filename = './unstructuredVideos/'+pathToSave+"/video%d.jpg" % count;count+=1
           print( filename )
           cv2.imwrite(filename, frame)
 
+        else:
+          mkdir('./unstructuredVideos/'+pathToSave)
+          
+          filename = './unstructuredVideos/'+pathToSave+"/video%d.jpg" % count;count+=1
+          print( filename )
+          cv2.imwrite(filename, frame)
+          
+
     cap.release()
-
-
-
-
-
 
 videoProcessor( pathFile )
