b775a1cc6707c31ab551383224188df2b3238914
Jose <ignacioalfaro99@gmail.com>
2021-09-26 17:11:12 -0600

    Cambios

--- videoProcessor.py
+++ videoProcessor.py
@@ -6,45 +6,45 @@ from os import mkdir
 
 
 
-pathFile = '/home/nacho/Desktop/videos/bb.mp4'
+pathFile = '/home/nacho/Desktop/videos/kof.mp4'
 
 def videoProcessor( pathFile ): 
     
     count = 0
     videoFile = pathFile
     cap = cv2.VideoCapture( videoFile )
-    frameRate =  cap.get(5)
+    frameRate =  cap.get( 5 )
 
     while( cap.isOpened() ):
 
-      frameId = cap.get(1)
+      frameId = cap.get( 1 )
       ret, frame = cap.read()
 
       if not ret:
         break
 
 
-      if (frameId % math.floor(frameRate) == 0):
+      if (frameId % math.floor( frameRate ) == 0):
 
         video_file_name = pathFile.split("/")[-1][0:-4]
         
-        if( os.path.isdir('./unstructuredVideos/' + video_file_name)):
+        if ( os.path.isdir('./unstructuredVideos/' + video_file_name) ):
           writeFile(video_file_name, count, frame)
           count+=1
         else:
 
           mkdir('./unstructuredVideos/' + video_file_name)
-          writeFile(video_file_name, count, frame)      
+          writeFile( video_file_name, count, frame )      
           count += 1
 
     cap.release()
 
 
 
-def writeFile( video_file_name,count, frame):
+def writeFile( video_file_name,count, frame ):
   filename = './unstructuredVideos/'+video_file_name+"/" + video_file_name +"%d.jpg" % count;
   print(filename)
-  cv2.imwrite(filename, frame)
+  cv2.imwrite( filename, frame )
 
 
 videoProcessor( pathFile )
